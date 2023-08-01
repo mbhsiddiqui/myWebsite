55d9a131af82eb25aabb3cfc8d672988a9d2ae44
Maaz Siddiqui <msiddiqi@my.yorku.ca>
2023-08-01 06:56:36 -0400

    fix hierarchy issues, add animations, add tech-stack section

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,18 @@
 const imgA = document.querySelector(".about-me__picture");
 const imgB = document.querySelector(".nav__img");
+const nav = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
   const rect = imgA.getBoundingClientRect();
   const navHeight = document.querySelector("nav").offsetHeight;
-  if (rect.top + rect.height <= navHeight) {
+  if (rect.top + 10 <= navHeight) {
     imgB.style.transform = "scale(1)";
+    nav.style.borderRadius = "2%";
+    nav.style.boxShadow = "0 2px 12px rgba(0, 0, 0, 0.3)";
   } else {
     imgB.style.transform = "scale(0)";
+    nav.style.borderRadius = "0%";
+    nav.style.boxShadow = "0 0 0";
   }
 });
 
